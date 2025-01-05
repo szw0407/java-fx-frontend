01e5a3d2b2883ef8972712cc2ff59f409709fb96
xqli <xqli>
2025-01-06 05:51:53 +0800

    first commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,26 @@
+module com.teach.javafx {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+    requires javafx.swing;
+    requires org.apache.pdfbox;
+    requires java.logging;
+    requires com.google.gson;
+    requires java.net.http;
+    requires spring.security.crypto;
+    requires javafx.media;
+
+    opens com.teach.javafx to javafx.fxml;
+    opens com.teach.javafx.request to com.google.gson, javafx.fxml;
+    opens com.teach.javafx.controller.base to com.google.gson, javafx.fxml;
+    opens com.teach.javafx.controller to com.google.gson, javafx.fxml;
+    opens com.teach.javafx.models to javafx.base,com.google.gson;
+    opens com.teach.javafx.util to com.google.gson, javafx.fxml;
+
+    exports com.teach.javafx;
+    exports com.teach.javafx.controller;
+    exports com.teach.javafx.controller.base;
+    exports com.teach.javafx.request;
+    exports com.teach.javafx.util;
+
+}
