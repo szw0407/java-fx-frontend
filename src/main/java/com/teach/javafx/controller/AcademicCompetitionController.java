208072c199b0b5e681d25ac264a1b97322226a35
zhenxin <180510364+AliceZhenX@users.noreply.github.com>
2025-05-31 14:10:13 +0800

    修正错误

--- src/main/java/com/teach/javafx/controller/AcademicCompetitionController.java
+++ src/main/java/com/teach/javafx/controller/AcademicCompetitionController.java
@@ -1,5 +1,3 @@
-//问题：1.如何设置下拉表单选择学生-类似成绩管理模块 2.如何初始化信息
-
 package com.teach.javafx.controller;
 
 import com.teach.javafx.AppStore;
@@ -7,18 +5,6 @@ import com.teach.javafx.MainApplication;
 import com.teach.javafx.controller.base.LocalDateStringConverter;
 import com.teach.javafx.controller.base.ToolController;
 import com.teach.javafx.request.*;
-import javafx.scene.Scene;
-import javafx.scene.control.cell.TextFieldTableCell;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.FlowPane;
-import javafx.stage.Modality;
-import javafx.stage.Stage;
-import com.teach.javafx.request.DataRequest;
-import com.teach.javafx.request.DataResponse;
-import com.teach.javafx.util.CommonMethod;
-import com.teach.javafx.controller.base.MessageDialog;
 import javafx.collections.FXCollections;
 import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
@@ -26,10 +12,11 @@ import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.MapValueFactory;
 import javafx.stage.FileChooser;
+import com.teach.javafx.util.CommonMethod;
+import com.teach.javafx.util.DateTimeTool;
+import com.teach.javafx.controller.base.MessageDialog;
 
-import java.io.ByteArrayInputStream;
-import java.io.File;
-import java.io.FileOutputStream;
+
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
@@ -37,8 +24,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-
-public class AcademicCompetitionController extends ToolController{
+public class AcademicCompetitionController extends ToolController {
     @FXML
     private TableView<Map> dataTableView;
 
@@ -51,11 +37,10 @@ public class AcademicCompetitionController extends ToolController{
     @FXML
     private TableColumn<Map, String> achievementColumn;
 
-
     @FXML
     private TextField studentIdField;
     @FXML
-    private DatePicker timeField;
+    private javafx.scene.control.DatePicker timeField;
     @FXML
     private TextField subjectField;
     @FXML
@@ -64,14 +49,21 @@ public class AcademicCompetitionController extends ToolController{
     @FXML
     private TextField searchField;
 
+    @FXML
+    private Button saveButton; // 新增保存按钮的引用
+    @FXML
+    private Button deleteButton; // 新增删除按钮的引用
+
+    @FXML
+    private Button addButton;
+
     private Integer id = null;
     private ArrayList<Map> competitionList = new ArrayList<>();
     private ObservableList<Map> competitionObservableList = FXCollections.observableArrayList();
 
-
-    private void setTableViewData(){
+    private void setTableViewData() {
         competitionObservableList.clear();
-        for(int j = 0; j < competitionList.size(); j++){
+        for(int j = 0; j < competitionList.size(); j++) {
             competitionObservableList.addAll(FXCollections.observableArrayList(competitionList.get(j)));
         }
         dataTableView.setItems(competitionObservableList);
@@ -79,7 +71,7 @@ public class AcademicCompetitionController extends ToolController{
 
     @FXML
     public void initialize() {
-        JwtResponse jwtResponse = AppStore. getJwt();
+        JwtResponse jwtResponse = AppStore.getJwt();
         String currentStudentId = String.valueOf(jwtResponse.getId());
         DataResponse res;
         DataRequest req = new DataRequest();
@@ -87,13 +79,15 @@ public class AcademicCompetitionController extends ToolController{
         res = HttpRequestUtil.request("/api/academicCompetition/getAcademicCompetitionList", req);
         if (res != null && res.getCode() == 0) {
             ArrayList<Map> filteredList = new ArrayList<>();
-            for(Map record : (ArrayList<Map>) res.getData()){
+            for(Map record : (ArrayList<Map>) res.getData()) {
                 if (currentStudentId.equals(String.valueOf(record.get("studentId")).replace(".0", ""))) {
                     filteredList.add(record);
                 }
             }
             competitionList = filteredList;
-            if (jwtResponse.getRole().equals("ROLE_ADMIN")) competitionList = (ArrayList<Map>) res.getData();
+            if (jwtResponse.getRole().equals("ROLE_ADMIN") || jwtResponse.getRole().equals("ROLE_TEACHER")) {
+                competitionList = (ArrayList<Map>) res.getData();
+            }
         }
         studentIdColumn.setCellValueFactory(new MapValueFactory<>("studentId"));
         timeColumn.setCellValueFactory(new MapValueFactory<>("time"));
@@ -106,17 +100,28 @@ public class AcademicCompetitionController extends ToolController{
         ObservableList<Integer> list = tsm.getSelectedIndices();
         list.addListener(this::onTableRowSelect);
         setTableViewData();
+
+        // 根据角色控制按钮的可用性
+        if (jwtResponse.getRole().equals("ROLE_STUDENT")) {
+            addButton.setDisable(true);
+            deleteButton.setDisable(true);
+            saveButton.setDisable(true);
+        } else {
+            addButton.setDisable(false);
+            saveButton.setDisable(false);
+            deleteButton.setDisable(false);
+        }
     }
 
-
-    public void clearPanel(){
+    public void clearPanel() {
         id = null;
         studentIdField.setText("");
         timeField.getEditor().setText("");
         subjectField.setText("");
         achievementField.setText("");
     }
-    public void changeStudentInfo(){
+
+    public void changeStudentInfo() {
         Map<String,Object> form = dataTableView.getSelectionModel().getSelectedItem();
         if (form == null) {
             clearPanel();
@@ -126,7 +131,7 @@ public class AcademicCompetitionController extends ToolController{
         DataRequest req = new DataRequest();
         req.add("id", id);
         DataResponse res = HttpRequestUtil.request("/api/academicCompetition/getAcademicCompetitionInfo", req);
-        if(res.getCode()!=0){
+        if(res.getCode()!=0) {
             MessageDialog.showDialog(res.getMsg());
             return;
         }
@@ -139,14 +144,13 @@ public class AcademicCompetitionController extends ToolController{
         achievementField.setText(CommonMethod.getString(form, "achievement"));
     }
 
-
     public void onTableRowSelect(ListChangeListener.Change<? extends Integer> change) {
         changeStudentInfo();
     }
 
     @FXML
     protected void onQueryButtonClick() {
-        JwtResponse jwtResponse = AppStore. getJwt();
+        JwtResponse jwtResponse = AppStore.getJwt();
         String currentStudentId = String.valueOf(jwtResponse.getId());
         String search = searchField.getText();
         DataRequest req = new DataRequest();
@@ -154,22 +158,26 @@ public class AcademicCompetitionController extends ToolController{
         DataResponse res = HttpRequestUtil.request("/api/academicCompetition/getAcademicCompetitionList", req);
         if (res != null && res.getCode() == 0) {
             ArrayList<Map> filteredList = new ArrayList<>();
-            for(Map record : (ArrayList<Map>) res.getData()){
+            for(Map record : (ArrayList<Map>) res.getData()) {
                 if (currentStudentId.equals(String.valueOf(record.get("studentId")).replace(".0", ""))) {
                     filteredList.add(record);
                 }
             }
             competitionList = filteredList;
-            if (jwtResponse.getRole().equals("ROLE_ADMIN")) competitionList = (ArrayList<Map>) res.getData();
+            if (jwtResponse.getRole().equals("ROLE_ADMIN") || jwtResponse.getRole().equals("ROLE_TEACHER")) {
+                competitionList = (ArrayList<Map>) res.getData();
+            }
             setTableViewData();
         }
     }
+
     @FXML
-    protected void onAddButtonClick(){
+    protected void onAddButtonClick() {
         clearPanel();
     }
+
     @FXML
-    protected void onDeleteButtonClick(){
+    protected void onDeleteButtonClick() {
         Map form = dataTableView.getSelectionModel().getSelectedItem();
         if (form == null) {
             MessageDialog.showDialog("没有选择，无法删除");
@@ -183,18 +191,19 @@ public class AcademicCompetitionController extends ToolController{
         DataRequest req = new DataRequest();
         req.add("id", id);
         DataResponse res = HttpRequestUtil.request("/api/academicCompetition/academicCompetitionDelete", req);
-        if(res!=null){
-            if(res.getCode()==0){
+        if(res!=null) {
+            if(res.getCode()==0) {
                 MessageDialog.showDialog("删除成功！");
                 onQueryButtonClick();
-            }else{
+            } else {
                 MessageDialog.showDialog(res.getMsg());
             }
         }
     }
+
     @FXML
-    protected void onSaveButtonClick(){
-        if(studentIdColumn.getText().isEmpty()){
+    protected void onSaveButtonClick() {
+        if(studentIdColumn.getText().isEmpty()) {
             MessageDialog.showDialog("学号为空，不能修改");
             return;
         }
@@ -208,18 +217,19 @@ public class AcademicCompetitionController extends ToolController{
         req.add("form",form);
         req.add("id",id);
         DataResponse res = HttpRequestUtil.request("/api/academicCompetition/academicCompetitionEditSave", req);
-        if(res.getCode()==0){
+        if(res.getCode()==0) {
             id = CommonMethod.getIntegerFromObject(res.getData());
             MessageDialog.showDialog("保存成功！");
             onQueryButtonClick();
-        }else{
+        } else {
             MessageDialog.showDialog(res.getMsg());
         }
     }
-    public void doNew()
-    {
+
+    public void doNew() {
         clearPanel();
     }
+
     public void doSave() {
         onSaveButtonClick();
     }
@@ -227,5 +237,4 @@ public class AcademicCompetitionController extends ToolController{
     public void doDelete() {
         onDeleteButtonClick();
     }
-
 }
